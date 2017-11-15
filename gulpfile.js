8f0ebefe60239c541f8b7a1c2ea91a87b28b7c17
Abdel Raoof <olakara@gmail.com>
2017-11-15 19:42:17 +0400

    minify css to single file

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,18 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
 
 var jsFiles = ['assets/js/jquery.min.js','assets/js/app.js'];
 var jsDestination = 'assets/js';
+var cssFiles = ['assets/css/bootstrap.css',
+                'assets/css/app.css',
+                'assets/css/colors.css',
+                'assets/css/github.css',
+                'assets/css/icomoon.css',
+                'assets/css/controls.css'
+              ];
+var cssDestination = 'assets/css'
 
 gulp.task('scripts', function() {
     
@@ -16,3 +25,13 @@ gulp.task('scripts', function() {
             .pipe(gulp.dest(jsDestination));
     
   });
+
+gulp.task('styles', function() {
+
+  return gulp.src(cssFiles)
+          .pipe(concat('styles.css'))
+          .pipe(gulp.dest(cssDestination))
+          .pipe(rename('styles.min.css'))
+          .pipe(minifyCSS())
+          .pipe(gulp.dest(cssDestination));
+});  
